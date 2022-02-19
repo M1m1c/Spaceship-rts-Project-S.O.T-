8450ee18ac5c3b9531ab1515f3d74d8750d836a4
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2022-02-19 15:01:43 +0100

    created prefab for RTS camera

--- Assets/Script/RTSCameraBase.cs
+++ Assets/Script/RTSCameraBase.cs
@@ -13,22 +13,39 @@ public class RTSCameraBase : MonoBehaviour
     //TODO make settings json or scriptable object used to set some fo these settings
 
     private Vector2 moveDirection = new Vector2();
-    private float moveSpeed = 0.5f;
+    private float moveSpeed = 0.3f;
 
     private Vector2 rotationDirection = new Vector2();
     private float rotationSpeed = 0.5f;
     private bool invertVerticalRot = false;
     private bool inverthorizontalRot = false;
+    private bool rotationToggle = false;
 
-    public void UpdateMoveDirection(InputAction.CallbackContext context)
+    private float zoomDirection = 0;
+    private float maxZoomOut = 200f;
+    private float zoomSpeed = 7f;
+    private float currentZoom = 5f;
+    public void InputMoveDirection(InputAction.CallbackContext context)
     {
         moveDirection = context.ReadValue<Vector2>();
     }
 
-    public void UpdateRotationDirection(InputAction.CallbackContext context)
+    public void InputRotationDirection(InputAction.CallbackContext context)
     {
         rotationDirection = context.ReadValue<Vector2>();
     }
+
+    public void InputRotationToggle(InputAction.CallbackContext context)
+    {
+        if (context.performed) { rotationToggle = true; }
+        else if (context.canceled) { rotationToggle = false; }
+    }
+
+    public void InputZoomDirection(InputAction.CallbackContext context)
+    {
+        zoomDirection = context.ReadValue<float>();
+    }
+
 
     private void OnValidate()
     {
@@ -51,30 +68,42 @@ public class RTSCameraBase : MonoBehaviour
         {
             CameraHolderRef = transform.GetChild(0).GetChild(0);
             CameraHolderRef.transform.localPosition = new Vector3(0f, 0f, -defaultArmLength);
+            currentZoom = defaultArmLength;
         }
 
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        var forwardMovement = transform.forward * moveDirection.y * moveSpeed;
-        var sideMovement = transform.right * moveDirection.x * moveSpeed;
+        var currentMoveSpeed = moveSpeed * Mathf.Sqrt(currentZoom);
+        var forwardMovement = transform.forward * moveDirection.y * currentMoveSpeed;
+        var sideMovement = transform.right * moveDirection.x * currentMoveSpeed;
         transform.Translate(forwardMovement + sideMovement, Space.World);
 
-        var rotX = inverthorizontalRot ? rotationDirection.x : -rotationDirection.x;
-        transform.Rotate(new Vector3(0f, rotX, 0f));
-
-        if (CameraPivotRef)
+        if (rotationToggle)
         {
-            var rotY = invertVerticalRot ? rotationDirection.y : -rotationDirection.y;
-            var verticalRot = Quaternion.Euler(CameraPivotRef.eulerAngles.x + rotY * rotationSpeed, 0f, 0f);
-            CameraPivotRef.localRotation = verticalRot;
+            var rotX = inverthorizontalRot ? -rotationDirection.x : rotationDirection.x;
+            transform.Rotate(new Vector3(0f, rotX, 0f));
+
+            if (CameraPivotRef)
+            {
+                var rotY = invertVerticalRot ? rotationDirection.y : -rotationDirection.y;
+                var verticalRot = Quaternion.Euler(CameraPivotRef.eulerAngles.x + rotY * rotationSpeed, 0f, 0f);
+                CameraPivotRef.localRotation = verticalRot;
+            }
         }
-        //transform.position = transform.position + new Vector3(moveDirection.x,0f,moveDirection.y);
     }
 
+    private void Update()
+    {
+        if (CameraHolderRef)
+        {
+            currentZoom = Mathf.Clamp(Mathf.Abs(CameraHolderRef.localPosition.z) - zoomDirection*zoomSpeed * Time.deltaTime, 1f, maxZoomOut);
+            var zoomPos = new Vector3(CameraHolderRef.localPosition.x, CameraHolderRef.localPosition.y, -currentZoom);
+            CameraHolderRef.localPosition = zoomPos;
+        }
+    }
 
 
     private void OnDrawGizmos()
