4d13a52b18e3d8f83e3ef6307cf1910c830a211f
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2022-03-04 13:30:29 +0100

    added fixed static funcitons, one regular and one distance based

--- Assets/Script/Utility/VelocityCalc.cs
+++ Assets/Script/Utility/VelocityCalc.cs
@@ -9,4 +9,18 @@ public static class VelocityCalc
         var velocityChange = (changeCondition ? Time.deltaTime * accelerationSpeed : deceleration);
         return velocityChange;
     }
+    public static float GetVelocityChangeFixed(float velocity, float decelerationSpeed, float accelerationSpeed, bool changeCondition)
+    {
+        var proportionalDec = -(Time.fixedDeltaTime + (Time.fixedDeltaTime * (decelerationSpeed * velocity)));
+        var deceleration = velocity > 0f ? proportionalDec : -Time.fixedDeltaTime;
+        var velocityChange = (changeCondition ? Time.fixedDeltaTime * accelerationSpeed : deceleration);
+        return velocityChange;
+    }
+    public static float GetVelocityChangeBasedOnDistFixed(float velocity, float decelerationSpeed, float decelDist, float accelerationSpeed, bool changeCondition)
+    {
+        var proportionalDec = -((velocity * velocity) / (decelerationSpeed * decelDist - velocity * (Time.fixedDeltaTime)));
+        var deceleration = velocity > 0f ? proportionalDec : -Time.fixedDeltaTime;
+        var velocityChange = (changeCondition ? Time.fixedDeltaTime * accelerationSpeed : deceleration);
+        return velocityChange;
+    }
 }
