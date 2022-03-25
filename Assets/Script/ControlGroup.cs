96846e0d856a90484b9975e13d5d7828a22d78cf
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2022-03-25 15:40:22 +0100

    Added control group and formation group cs scripts

---
+++ Assets/Script/ControlGroup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Used for storing a selection of units on one of the number keys.
+public class ControlGroup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/FormationGroup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//used for similar space ships to form formations and travel together
+public class FormationGroup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
