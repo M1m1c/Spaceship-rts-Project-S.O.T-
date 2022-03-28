be8fc4fc37bd7647629c817720021d237f8f4538
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2022-03-28 17:46:40 +0200

    fixed error when reciving invalid float for speed

--- Assets/Script/LightShipMovementComp.cs
+++ Assets/Script/LightShipMovementComp.cs
@@ -114,14 +114,14 @@ public class LightShipMovementComp : UnitMovement
         if (distanceToTarget < 1f)
         {
             //TODO fix when vertical move is longer that it does not rotate correctly at the end of movement
-            targetRotation = finalRotation;
-            //TODO when done rotationg to final rotation remove target
+            targetRotation = finalRotation;     
         }
         else
         {
             var rotationDirection = Vector3.RotateTowards(transform.forward, targetDirection, 360f, 1f);
             targetRotation = Quaternion.LookRotation(rotationDirection);
         }
+
 
         var rotVel = travelVelocity + (rotationVelocity);
         var rotSpeed = travelSpeed * (rotationSpeed * rotationModifier);
@@ -130,6 +130,11 @@ public class LightShipMovementComp : UnitMovement
             targetRotation,
             Time.fixedDeltaTime * travelSpeed + (rotSpeed * rotVel));
 
+        //TODO when done rotationg to final rotation remove target
+        //if (transform.rotation==finalRotation && reachedHorizontalTarget && reachedVerticalTarget) 
+        //{ 
+        //    Destroy(target.gameObject); 
+        //}
 
         if (reachedVerticalTarget && reachedHorizontalTarget) { return; }
 
@@ -168,24 +173,37 @@ public class LightShipMovementComp : UnitMovement
         {
             var verticalDir = new Vector3(0f, targetDirection.y, 0f).normalized;
             var vertSpeed = (travelSpeed * (travelVelocity * travelModifier) * vertSpeedMod) * Time.fixedDeltaTime;
-            transform.position += verticalDir * vertSpeed;
 
-            if (verticalDistance < vertSpeed + reachTargetAdditive)
+            if (!float.IsNaN(vertSpeed))
             {
-                reachedVerticalTarget = true;
+                transform.position += verticalDir * vertSpeed;
+
+                if (verticalDistance < vertSpeed + reachTargetAdditive)
+                {
+                    reachedVerticalTarget = true;
+                }
+
             }
+            else { reachedVerticalTarget = true; }
         }
 
         if (!reachedHorizontalTarget)
         {
             var horizontalDir = new Vector3(transform.forward.x, 0f, transform.forward.z).normalized;
             var horiSpeed = (travelSpeed * (travelVelocity * travelModifier) * horiSpeedMod) * Time.fixedDeltaTime;
-            transform.position += horizontalDir * horiSpeed;
-            
-            if (horizontalDistance < horiSpeed + reachTargetAdditive)
+
+            if (!float.IsNaN(horiSpeed))
             {
-                reachedHorizontalTarget = true;
+                transform.position += horizontalDir * horiSpeed;
+
+                if (horizontalDistance < horiSpeed + reachTargetAdditive)
+                {
+                    reachedHorizontalTarget = true;
+                }
             }
+            else { reachedHorizontalTarget = true; }
+
+
         }
 
         if (reachedVerticalTarget && reachedHorizontalTarget) { transform.position = target.position; }
