a4cc45e7d43a83e74a2e8cf47535c8851936d2c8
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2022-03-31 12:23:33 +0200

    Created IOrderable interface

---
+++ Assets/Script/IOrderable.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IOrderable 
+{
+    //TODO change this from transform to aoibject that contains a transform and a order type
+    public abstract Transform TargetOrderBeacon
+    {
+        get;
+        set;
+    }
+}
