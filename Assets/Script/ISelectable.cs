d3cb11207326b7913d81e1b99782c400c89d9a4e
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2022-03-31 12:23:51 +0200

    Created ISelectable Interface

---
+++ Assets/Script/ISelectable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISelectable 
+{
+    public abstract void OnSelected();
+   
+
+    public abstract void DeSelect();
+    
+}
