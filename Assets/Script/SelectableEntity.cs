edce4c8ef76d2c2163d372dbd609272dcad2f134
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2022-03-31 13:37:52 +0200

    implemented Iselectable

--- Assets/Script/SelectableEntity.cs
+++ Assets/Script/SelectableEntity.cs
@@ -2,37 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SelectableEntity : MonoBehaviour
+public class SelectableEntity : MonoBehaviour, ISelectable
 {
-
-    private Transform myOrderBeacon;
-
-    public Transform MyOrderBeacon
-    {
-        get { return myOrderBeacon; }
-        set
-        {
-            myOrderBeacon = value;
-
-            if (movementComp)
-            {
-                movementComp.Target = myOrderBeacon;
-            }
-        }
-    }
-
-    public Material LineMaterial;
-
     private Color startcol;
     private Renderer myRenderer;
 
-    private UnitMovement movementComp;
+    public IOrderable OrderableRoot { get; private set; }
 
     void Start()
     {
+        OrderableRoot = GetComponentInParent<IOrderable>();
+
         myRenderer = gameObject.GetComponent<Renderer>();
-        startcol = myRenderer.material.color;
-        movementComp = GetComponentInParent<UnitMovement>();
+        if (myRenderer) { startcol = myRenderer.material.color; }
     }
 
     public void OnSelected()
