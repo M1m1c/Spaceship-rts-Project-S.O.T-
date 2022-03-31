9172f273ad10454b62519c714621684919f3fc44
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2022-03-31 16:06:26 +0200

    forwarded reference to unit comp to unit formation on setup

--- Assets/Script/FormationButton.cs
+++ Assets/Script/FormationButton.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 public class FormationButton : UIButton
 {
@@ -10,12 +11,38 @@ public class FormationButton : UIButton
         var selection = selectionController.CurrentGroupOrigin.SelectionGroup;
         if (selection == null || (selection != null && selection.Count < 1)) { return; }
 
+        Dictionary<int, UnitComp> unitComps = new Dictionary<int, UnitComp>();
+        foreach (var item in selection)
+        {
+            var unitcomp = item.Value.OrderableComp as UnitComp;
+            if (unitcomp == null)
+            {
+                selection.Remove(item.Key);
+                continue;
+            }
+            unitComps.Add(item.Key, unitcomp);
+        }
+
+        Dictionary<int, SelectableUnit> unitSelections = new Dictionary<int, SelectableUnit>();
+        foreach (var item in selection)
+        {
+            var value = (SelectableUnit)item.Value;
+            if (value)
+            {
+                unitSelections.Add(item.Key, value);
+            }
+        }
+
         var spawnPos = selectionController.CurrentGroupOrigin.transform.position;
         //TODO check if there already is a formation group, so we dont need to spawn one
         //TODO take into account max units in formation,
         //and spawn multiple formation groups if a larger amount is selected.
         var group = Instantiate(FormationGroupPrefab, spawnPos, Quaternion.identity);
 
-        group.Setup(selection, FormationSetting);
+        group.Setup(unitSelections, unitComps, FormationSetting);
+
+        var collection = selectionController.GetSelectionCollection;
+        collection.DeselectAllEntities();
+        collection.AddSelectedEntity(group);
     }
 }
