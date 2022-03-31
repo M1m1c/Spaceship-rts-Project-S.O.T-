67065469c5bdd927c946e6f3ed6e2f2663571ac3
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2022-03-31 16:01:24 +0200

    added referecen to grouping slot comp

--- Assets/Script/UnitComp.cs
+++ Assets/Script/UnitComp.cs
@@ -1,26 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 //TODO this is supposed to function as a hub that can handle orders, both movement and attack orders
 public class UnitComp : MonoBehaviour, IOrderable
 {
     private Transform myOrderBeacon;
-    public Transform TargetOrderBeacon 
+    public Transform TargetOrderBeacon
     {
-        get { return myOrderBeacon; } 
-        set 
-        { 
-            myOrderBeacon = value; 
-            MovementComp.TargetBeacon = value;
-        } 
+        get { return myOrderBeacon; }
+        set
+        {
+            myOrderBeacon = value;
+
+                MovementComp.TargetBeacon = value;
+            //if (!groupSlot.MyFormationGroup)
+            //{
+            //}
+            //else
+            //{
+            //    groupSlot.MyFormationGroup.TargetOrderBeacon = value;
+            //}
+        }
     }
 
+    //public FormationGroup MyFormationGroup { get; set; }
+
     public UnitMovement MovementComp { get; private set; }
+
+    public Transform RootTransform => transform;
+
+    public GroupingSlotComp GroupingSlot => groupSlot;
+
+    private GroupingSlotComp groupSlot;
 
     void Start()
     {
         MovementComp = GetComponent<UnitMovement>();
+        Assert.IsNotNull(MovementComp);
+
+        groupSlot = GetComponent<GroupingSlotComp>();
+        Assert.IsNotNull(groupSlot);
     }
 
 }
