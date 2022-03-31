831928079b65a3b955b1d20014bcd8d8d7ca58f5
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2022-03-31 15:59:55 +0200

    added root transfrom getter

--- Assets/Script/IOrderable.cs
+++ Assets/Script/IOrderable.cs
@@ -2,12 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IOrderable 
+public interface IOrderable
 {
     //TODO change this from transform to aoibject that contains a transform and a order type
-    public abstract Transform TargetOrderBeacon
-    {
-        get;
-        set;
-    }
+    public abstract Transform TargetOrderBeacon { get; set; }
+    public abstract Transform RootTransform { get; }
 }
